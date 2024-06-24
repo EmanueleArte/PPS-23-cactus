097d902297a8a20475e2285371b65db32fcf0506
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-06-24 21:17:55 +0200

    Add tests for coverable cards

--- src/test/scala/card/CardTests.scala
+++ src/test/scala/card/CardTests.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CardTests extends AnyFlatSpec:
-  def card: Card = GenericCard(10, PokerSuit.Spades)
+  val card: Card = GenericCard(10, PokerSuit.Spades)
 
   "A Card" should "have an integer value" in:
     card.value shouldBe a [Int]
@@ -20,5 +20,18 @@ class CardTests extends AnyFlatSpec:
     twoOfClubs.value should be (2)
     twoOfClubs.suit should be (PokerSuit.Clubs)
 
-  
+  val coverableCard: Card & Coverable = new GenericCard(5, PokerSuit.Diamonds) with Coverable
+
+  "A coverable card" should "be covered by default" in:
+    coverableCard.isCovered should be (true)
+
+  "A coverable card" should "be able to be uncovered" in:
+    coverableCard.isCovered should be (true)
+    coverableCard.uncover()
+    coverableCard.isCovered should be (false)
+
+  "A coverable card" should "be able to be covered" in:
+    coverableCard.isCovered should be (false)
+    coverableCard.cover()
+    coverableCard.isCovered should be (true)
 
