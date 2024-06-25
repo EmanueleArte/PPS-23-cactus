74f87bc9f2691bc8d44610c823f597270756ea3c
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-06-25 15:28:30 +0200

    Add tests for card creation DSL

--- src/test/scala/card/PokerCardTests.scala
+++ src/test/scala/card/PokerCardTests.scala
@@ -1,7 +1,10 @@
 package card
 
 import card.CardsData.*
+import PokerSuit.*
 import card.Cards.*
+import card.CardBuilder.PokerCardNames.*
+import card.CardBuilder.PokerDSL.*
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -9,6 +12,15 @@ class PokerCardTests extends AnyFlatSpec:
 
   "Poker card" should "have an value between 1 to 13" in:
     intercept[IllegalArgumentException]:
-      PokerCard(-3, PokerSuit.Spades)
+      PokerCard(-3, Spades)
     intercept[IllegalArgumentException]:
-      PokerCard(14, PokerSuit.Spades)
+      PokerCard(14, Spades)
+
+  "Poker card created with DSL" should "have same value and suit as one created using constructor" in:
+    10 of Spades should be (PokerCard(10, Spades))
+
+  "Poker card created with poker names" should "have same value and suit as one created using constructor" in:
+    Ace of Clubs should be (PokerCard(1, Clubs))
+    Jack of Diamonds should be (PokerCard(11, Diamonds))
+    Queen of Hearts should be (PokerCard(12, Hearts))
+    King of Spades should be (PokerCard(13, Spades))
