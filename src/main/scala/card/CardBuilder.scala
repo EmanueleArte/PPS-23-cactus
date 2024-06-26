e66cb9b43abccaa35e28cefb3ecd1dbbd0dca341
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-06-26 13:19:34 +0200

    Add method to create coverable poker cards in DSL

--- src/main/scala/card/CardBuilder.scala
+++ src/main/scala/card/CardBuilder.scala
@@ -16,3 +16,12 @@ object CardBuilder:
        * @return a poker card with the given value and suit
        */
       def of(suit: PokerSuit): PokerCard = PokerCard(value, suit)
+
+      /**
+       * Creates a coverable poker card with the given value and suit. Using the syntax `value OF suit`
+       * (e.g. 5 OF Spades).
+       *
+       * @param suit the suit of the card
+       * @return a coverable poker card with the given value and suit
+       */
+      def OF(suit: PokerSuit): PokerCard & Coverable = new PokerCard(value, suit) with Coverable
