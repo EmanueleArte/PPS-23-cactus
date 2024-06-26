381057034b16cffd7a055bd19ace5659e27f2080
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-06-26 13:18:00 +0200

    Add test for coverable poker cards created with DSL

--- src/test/scala/card/PokerCardTests.scala
+++ src/test/scala/card/PokerCardTests.scala
@@ -25,3 +25,12 @@ class PokerCardTests extends AnyFlatSpec:
     Jack of Diamonds should be (PokerCard(11, Diamonds))
     Queen of Hearts should be (PokerCard(12, Hearts))
     King of Spades should be (PokerCard(13, Spades))
+
+  "Coverable Poker card created with DSL" should "be covered when created" in:
+    (3 OF Clubs).isCovered should be (true)
+
+  it should "be uncovered after it has been uncovered" in:
+    val card = 3 OF Clubs
+    card.isCovered should be (true)
+    card.uncover()
+    card.isCovered should be (false)
