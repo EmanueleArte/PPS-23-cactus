59e3c49078df6766c12ece83d19d7fb10ad81002
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2024-06-27 15:18:53 +0200

    Create generic pile

--- src/main/scala/model/deck/Piles.scala
+++ src/main/scala/model/deck/Piles.scala
@@ -16,9 +16,24 @@ object Piles:
 
     def empty(): DiscardPile
 
+  object DiscardPile:
+    def apply(): DiscardPile = GenericPile()
 
   abstract class PileImpl() extends DiscardPile:
     override def size: Int = cards.size
+
+  case class GenericPile(cards: List[Card]) extends PileImpl:
+    override type CardType = Card
+    override def put(card: Card): DiscardPile = card match
+      case card: Card => GenericPile(card +: cards)
+      case _ => this
+
+    override def draw(): Option[Card] = cards.headOption
+
+    override def empty(): DiscardPile = GenericPile()
+  object GenericPile:
+    def apply(): GenericPile = GenericPile(List())
+
 
   @SuppressWarnings(Array("org.wartremover.warts.All"))
   case class PokerPile(cards: List[PokerCard]) extends PileImpl:
@@ -29,10 +44,10 @@ object Piles:
     override def put(card: Card): DiscardPile =
       require(card.isInstanceOf[PokerCard], "Expected a PokerCard")
       card match
-        case pokerCard: CardType => PokerPile(pokerCard +: cards)
+        case pokerCard: PokerCard => PokerPile(pokerCard +: cards)
         case _ => this
 
-    override def empty(): DiscardPile = PokerPile(List())
+    override def empty(): DiscardPile = PokerPile()
 
   object PokerPile:
     def apply(): PokerPile = PokerPile(List())
