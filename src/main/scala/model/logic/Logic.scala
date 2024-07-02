f7888dd3b81a7c423cfc5305141ad0bb69af60a1
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-02 18:40:35 +0200

    Create a generic turn-based game logic

---
+++ src/main/scala/model/logic/Logic.scala
@@ -0,0 +1,55 @@
+package model.logic
+
+import model.utils.Iterator.PeekableIterator
+import scala.annotation.tailrec
+
+/** Logic of a game. */
+object Logic:
+
+  /** Temporary implementation of a player. */
+  trait Player:
+    def name: String
+
+  /** Logic of a generic turn based game. */
+  trait Logic:
+    type Score
+
+    protected val _players: List[Player]          = List()
+    protected val _currentPlayer: PeekableIterator[Player] = PeekableIterator(Iterator.continually(_players).flatten)
+
+    /** Represents all the actions action done during the turn. */
+    def playTurn(): Unit
+
+    /**
+     * Check if the game is over.
+     *
+     * @return `true` if the game is over, `false` otherwise.
+     */
+    def isGameOver: Boolean
+
+    /**
+     * Calculate the score of the game.
+     *
+     * @return a map with the [[Player]] and the score.
+     */
+    def calculateScore: Map[Player, Score]
+
+    /** Main loop of the game. */
+    def gameLoop(): Unit
+
+  /**
+   * Abstract implementation of a [[Logic]] that provides implementations of basic methods.
+   *
+   * @param players list of players in the game.
+   */
+  abstract class AbstractLogic(players: List[Player]) extends Logic:
+    type Score = Int
+
+    override protected val _players: List[Player] = players
+
+    @tailrec
+    override final def gameLoop(): Unit =
+      if !isGameOver then
+        playTurn()
+        _currentPlayer.next()
+        gameLoop()
