951d8df2a75b030b3f13362b053dfada91bbebd2
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-04 09:47:43 +0200

    Add cactus logic setup

--- src/main/scala/model/logic/Logics.scala
+++ src/main/scala/model/logic/Logics.scala
@@ -8,13 +8,13 @@ import scala.annotation.tailrec
 
 /** Logic of a game. */
 object Logics:
+  type Players = List[Player]
 
   /** Logic of a generic turn based game. */
   trait Logic:
     type Score
-    type Players = List[Player]
-    
-    protected val _players: Players = List()
+
+    protected val _players: Players                        = List()
     protected val _currentPlayer: PeekableIterator[Player] = PeekableIterator(Iterator.continually(_players).flatten)
 
     /** Represents all the actions action done during the turn. */
@@ -40,7 +40,7 @@ object Logics:
   /**
    * Abstract implementation of a [[Logic]] that provides implementations of basic methods.
    *
-   * @param nPlayers list of players in the game.
+   * @param nPlayers number of players in the game.
    */
   abstract class AbstractLogic(nPlayers: Int) extends Logic:
     type Score = Int
@@ -58,15 +58,27 @@ object Logics:
     val game: Game
 
   /** Trait that represents a game logic based on a certain game. */
-  trait GameLogic extends GameProvider
+  trait GameLogic extends GameProvider:
+    /** Setup the game.
+     *
+     * @param nPlayers number of players in the game.
+     * @return the list of players obtained after the setup.
+     */
+    def setup(nPlayers: Int): Players = game.setupGame(nPlayers)
 
+  /**
+   * Logic of the Cactus game.
+   *
+   * @param nPlayers number of players in the game.
+   */
   class CactusLogic(nPlayers: Int) extends AbstractLogic(nPlayers) with GameLogic:
     type Score = Int
 
     override val game: Game = CactusGame()
+    override val _players: Players = setup(nPlayers)
 
     override def playTurn(): Unit = None
 
     override def isGameOver: Boolean = true
 
-    override def calculateScore: Map[Player, Score] = Map()
+    override def calculateScore: Map[Player, Score] = _players.map(p => p -> 0).toMap
