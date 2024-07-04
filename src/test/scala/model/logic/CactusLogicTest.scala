c6a429e8b0b0835b8095e52ad80d873784076fcf
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-04 11:17:39 +0200

    Add test for initial turn move (draw from deck)

--- src/test/scala/model/logic/CactusLogicTest.scala
+++ src/test/scala/model/logic/CactusLogicTest.scala
@@ -1,6 +1,5 @@
 package model.logic
 
-import model.logic.*
 import model.logic.Logics.CactusLogic
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.flatspec.AnyFlatSpec
@@ -12,5 +11,11 @@ class CactusLogicTest extends AnyFlatSpec:
 
   "A Cactus Logic" should "have the correct number of players after the setup" in:
     val logic = CactusLogic(playersNumber)
-    logic.calculateScore.size should be (playersNumber)
-  
+    logic.players.size should be (playersNumber)
+
+  "Players" should "have 5 cards after draw from the deck" in:
+    val logic = CactusLogic(playersNumber)
+    for _ <- 1 to playersNumber do
+      logic.playTurn()
+      logic.playerIterator.next()
+    logic.players.foreach(player => player.cards.size should be (5))
