b0d7e4ad453edcd706b1db732ec26b0bbc7fe0fc
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2024-07-05 16:46:42 +0200

    Add javadoc

--- src/main/scala/model/game/Games.scala
+++ src/main/scala/model/game/Games.scala
@@ -5,25 +5,77 @@ import model.deck.Decks.{Deck, PokerDeck}
 import model.deck.Piles.{DiscardPile, PokerPile}
 import player.Players.{CactusPlayer, Player}
 
+/**
+ * An opaque type representing the scores of players in a game.
+ * Internally it is a [[ Map[Player, Int] ]].
+ */
 opaque type Scores = Map[Player, Int]
 
+/**
+ * Companion object of [[Scores]] opaque type.
+ * Provides methods to interact with `Scores` values.
+ */
 object Scores:
+  /**
+   * Creates a [[Scores]] from a [[ Map[Player, Int] ]].
+   *
+   * @param map the map of players and their scores.
+   * @return t [[Scores]] representing the provided map.
+   */
   def apply(map: Map[Player, Int]): Scores = map
-  def toMap(score: Scores): Map[Player, Int] = score
-  extension (score: Scores)
-    def size: Int = score.size
-    def get(player: Player): Option[Int] = score.get(player)
-    def players: Iterable[Player] = score.keys
-    def isEmpty: Boolean = score.isEmpty
+
+  /**
+   * Converts a [[Scores]] to a Score.
+   *
+   * @param scores the [[Scores]] to convert.
+   * @return the underlying [[Scores]] of the provided [[Scores]].
+   */
+  def toMap(scores: Scores): Map[Player, Int] = scores
+  extension (scores: Scores)
+
+    /**
+     * Returns the size of the provided [[Scores]].
+     *
+     * @return the number of player-score pairs in the [[Scores]].
+     */
+    def size: Int = scores.size
+
+    /**
+     * Retrieves the score associated with a specific player in this [[Scores]].
+     *
+     * @param player The player whose score is to be retrieved.
+     * @return an [[ Option[Int] ]] containing the score of the player if present, or `None` if the player is not in this [[Scores]].
+     */
+    def get(player: Player): Option[Int] = scores.get(player)
+
+    /**
+     * Retrieves the players contained in the [[Scores]].
+     *
+     * @return an [[ Iterable[Player] ]] containing the keys of the Score associated to [[Scores]].
+     */
+    def players: Iterable[Player] = scores.keys
+
+    /**
+     * Checks if the provided [[Scores]] is empty.
+     *
+     * @return `true` if the [[Scores]] is empty, `false` otherwise.
+     */
+    def isEmpty: Boolean = scores.isEmpty
 
 /** Generic card game. */
 trait Game:
   /**
-   * Setup method to call before start the game.
+   * Setups method to call before start the game.
    * @param playersNumber number of players in the match.
    * @return a list with the initialized players.
    */
   def setupGame(playersNumber: Int): List[Player]
+
+  /**
+   * Calculate the scores for each player.
+   * @param players list of players to which calculate the scores.
+   * @return a [[Scores]] with the scores for each player.
+   */
   def calculateScores(players: List[Player]): Scores
 
 /** Cactus game implementation. */
@@ -43,15 +95,15 @@ case class CactusGame() extends Game:
       .map(_ => (1 to 4).toList.map(_ => deck.draw().get))
       .map(list => CactusPlayer(list))
   override def calculateScores(players: List[Player]): Scores = Scores(
-    players
-      .zipWithIndex
+    players.zipWithIndex
       .map((player, index) => (player, player.cards))
-      .filter((player, cards) => cards.count {
-        case card: PokerCard => true
-        case _ => false
-      } == cards.size)
+      .filter((player, cards) =>
+        cards.count {
+          case card: PokerCard => true
+          case _               => false
+        } == cards.size
+      )
       .map((player, cards) => (player, cards.collect { case card: PokerCard => card.value }.sum))
       .map((player, score) => player -> score)
       .toMap
-    )
-
+  )
