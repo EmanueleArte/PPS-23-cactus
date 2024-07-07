8c022ef361510f2e368d0a0ac6be7efe4fde5346
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2024-07-07 11:33:08 +0200

    Create trait Drawable and make Deck and DiscardPile extend it

---
+++ src/main/scala/model/deck/Drawable.scala
@@ -0,0 +1,16 @@
+package model.deck
+
+import card.Cards.Card
+
+
+/**
+ * Represents an object from which is possible to draw something.
+ *
+ * @tparam C type of the drawn item. C needs to be at least a [[Card]].
+ */
+trait Drawable[C <: Card]:
+  /**
+   * Picks the first item of the collection.
+   * @return an `Option` with the drawn item.
+   */
+  def draw(): Option[C]
