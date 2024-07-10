ab0992c8ae8370844eab2b7169f560d97fe11055
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2024-07-10 11:58:35 +0200

    add unimplemented methods

--- src/main/scala/bot/Bots.scala
+++ src/main/scala/bot/Bots.scala
@@ -1,6 +1,7 @@
 package bot
 
 import card.Cards.Card
+import model.deck.Drawable
 import player.Players.CactusPlayer
 
 /** The bots of the game */
@@ -13,15 +14,30 @@ object Bots:
     var knownCards: List[Card]
 
     /** Let the [[CactusBot]] see a [[Card]].
- *
+     *
      * @param cardIndex the index of the [[Card]] in the list of the cards.
      */
     def seeCard(cardIndex: Int): Unit;
 
-    /** Removes the [[Card]] from the known cards list.
-     * @param card the card to remove
+    def chooseDraw(deck: Boolean): Drawable[_ <: Card]
+
+    /** Discards a card from the player's hand.
+     * @param cardIndex the index of the card in the list to discard
+     * @return the discarded card
      */
-    def removeFromKnownCards(card: Card): Unit
+    def discard(cardIndex: Int): Card
+
+    /*-checkEffect()*/
+    def discardWithMalus(cardIndex: Int): Card
+
+    def callCactus(): Unit
+
+    def chooseOwnCard(cardIndex: Int): Card
+
+    //def chooseTwoCards((Player, card: int), (Player, card: int))
+
+    def choosePlayer(player: CactusPlayer): CactusPlayer
+
 
   @SuppressWarnings(Array("org.wartremover.warts.All"))
   class CactusBotImpl(cards: List[Card]) extends CactusPlayer(cards) with CactusBot:
@@ -30,5 +46,20 @@ object Bots:
     override def seeCard(cardIndex: Int): Unit =
       knownCards = knownCards :+ cards(cardIndex)
 
-    override def removeFromKnownCards(card: Card): Unit =
+    private def removeFromKnownCards(card: Card): Unit =
       knownCards = knownCards.filterNot(c => c == card)
+
+    override def discard(cardIndex: Int): Card =
+      val discardedCard = super.discard(cardIndex)
+      removeFromKnownCards(discardedCard)
+      discardedCard
+
+    override def chooseDraw(deck: Boolean): Drawable[_ <: Card] = ???
+
+    override def discardWithMalus(cardIndex: Int): Card = ???
+
+    override def callCactus(): Unit = ???
+
+    override def chooseOwnCard(cardIndex: Int): Card = ???
+
+    override def choosePlayer(player: CactusPlayer): CactusPlayer = ???
