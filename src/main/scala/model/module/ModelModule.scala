b6f35dcc7732ac1badd752b04150165700335560
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-12 14:02:14 +0200

    Create model module trait

--- src/main/scala/model/module/ModelModule.scala
+++ src/main/scala/model/module/ModelModule.scala
@@ -1,15 +1,12 @@
-package model
+package model.module
 
 import model.logic.Logics.Logic
 
 /** Represents the model component. */
-object ModelModule:
+trait ModelModule:
   /** Type representing the model of the game. */
   type ModelType <: Logic
-  
-  /** Provider a [[Model]]. */
+
+  /** Provider for a [[Model]]. */
   trait Provider:
     val model: ModelType
-
-  /** Interface of the game. */
-  trait Interface extends Provider
