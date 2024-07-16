7f80d5361ae7ae397d373dee492e70445bf7285f
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-16 17:52:13 +0200

    Add simple gui for main menu

--- src/main/scala/view/module/menu/MainMenuViewModule.scala
+++ src/main/scala/view/module/menu/MainMenuViewModule.scala
@@ -1,18 +1,22 @@
 package view.module.menu
 
+import control.module.menu.MainMenuControllerModule
+import model.game.GamesList
 import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Pos
 import scalafx.scene.Scene
-import scalafx.scene.control.{Label, ListView}
+import scalafx.scene.control.{ComboBox, Label, ListView}
 import scalafx.scene.layout.VBox
+import scalafx.stage.Screen
 import view.module.ViewModule
 
 /** Represents the view module for the menu. */
 object MainMenuViewModule extends ViewModule:
   override type ViewType = JFXApp3
 
-  override type Requirements = Int
+  override type Requirements = MainMenuControllerModule.Provider
 
   /** Represents the view component for the menu. */
   trait Component:
@@ -20,18 +24,23 @@ object MainMenuViewModule extends ViewModule:
 
     class MainMenuView extends JFXApp3:
       override def start(): Unit =
-        stage = new JFXApp3.PrimaryStage:
+        val primaryScreenBounds = Screen.primary.visualBounds
+        stage = new PrimaryStage:
           title.value = "Main Menu"
+          width = primaryScreenBounds.width
+          height = primaryScreenBounds.height
           scene = new Scene:
             root = new VBox:
               alignment = Pos.Center
               spacing = 10
               children = Seq(
-                new Label("Main Menu"):
-                  style = "-fx-font-size: 24pt; -fx-text-alignment: center;"
-                , new ListView[String]:
-                  items = ObservableBuffer("Option 1", "Option 2", "Option 3")
-                  prefHeight = 150
+                new Label("Cactus & Co."):
+                  style = "-fx-font-size: 60pt; -fx-font-weight: bold; -fx-text-alignment: center;"
+                ,
+                new ComboBox[String]:
+                  items = ObservableBuffer.from(GamesList.games)
+                  promptText = "Select a game"
+                  prefWidth = 200
               )
 
   /** Interface of the view module of the menu. */
