2a4031d91fff5c0f895edf79b1979b17d4ebd9c0
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-16 18:17:23 +0200

    Add basic template

--- src/main/scala/view/module/menu/MainMenuViewModule.scala
+++ src/main/scala/view/module/menu/MainMenuViewModule.scala
@@ -1,14 +1,15 @@
 package view.module.menu
 
 import control.module.menu.MainMenuControllerModule
+import model.bot.CactusBotsData
 import model.game.GamesList
-import scalafx.application.JFXApp3
+import scalafx.application.{JFXApp3, Platform}
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.collections.ObservableBuffer
-import scalafx.geometry.Pos
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.control.{ComboBox, Label, ListView}
-import scalafx.scene.layout.VBox
+import scalafx.scene.control.{Button, ComboBox, Label, ListView, Spinner}
+import scalafx.scene.layout.{HBox, VBox}
 import scalafx.stage.Screen
 import view.module.ViewModule
 
@@ -22,7 +23,8 @@ object MainMenuViewModule extends ViewModule:
   trait Component:
     context: Requirements =>
 
-    class MainMenuView extends JFXApp3:
+    /** Implementation of the main menu view using ScalaFx. */
+    class MainMenuFxView extends JFXApp3:
       override def start(): Unit =
         val primaryScreenBounds = Screen.primary.visualBounds
         stage = new PrimaryStage:
@@ -32,15 +34,60 @@ object MainMenuViewModule extends ViewModule:
           scene = new Scene:
             root = new VBox:
               alignment = Pos.Center
-              spacing = 10
+              spacing = 20
               children = Seq(
                 new Label("Cactus & Co."):
                   style = "-fx-font-size: 60pt; -fx-font-weight: bold; -fx-text-alignment: center;"
+                  margin = new scalafx.geometry.Insets(Insets(0, 0, 50, 0))
                 ,
                 new ComboBox[String]:
                   items = ObservableBuffer.from(GamesList.games)
                   promptText = "Select a game"
                   prefWidth = 200
+                ,
+                new HBox:
+                  alignment = Pos.Center
+                  spacing = 10
+                  children = Seq(
+                    new Label("Number of players:"),
+                    new Spinner[Int](2, 6, 2):
+                      editable = false
+                      prefWidth = 200
+                  )
+                ,
+                new HBox:
+                  alignment = Pos.Center
+                  spacing = 50
+                  margin = new scalafx.geometry.Insets(Insets(0, 0, 30, 0))
+                  children = Seq(
+                    new VBox:
+                      alignment = Pos.Center
+                      spacing = 10
+                      children = Seq(
+                        new Label("Player 0:"),
+                        new Label("Draw method:"),
+                        new ComboBox[String]:
+                          items = ObservableBuffer.from(CactusBotsData.DrawMethods.values.map(_.toString))
+                          promptText = "Select a draw method"
+                          prefWidth = 200
+                        ,
+                        new Label("Discard method:"),
+                        new ComboBox[String]:
+                          items = ObservableBuffer.from(CactusBotsData.DiscardMethods.values.map(_.toString))
+                          promptText = "Select a discard method"
+                          prefWidth = 200
+                        ,
+                        new Label("Memory:"),
+                        new ComboBox[String]:
+                          items = ObservableBuffer.from(CactusBotsData.Memory.values.map(_.toString))
+                          promptText = "Select a memory quality"
+                          prefWidth = 200
+                      )
+                  )
+                ,
+                new Button:
+                  text = "Start game"
+                  onAction = _ => Platform.exit()
               )
 
   /** Interface of the view module of the menu. */
