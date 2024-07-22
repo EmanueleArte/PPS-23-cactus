fdae8ebcd1b7db7b2f13db2991a8c6a5485cbb91
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-22 18:52:57 +0200

    Refactor GamesList to PlayableGame

--- src/main/scala/model/game/GamesList.scala
+++
@@ -1,5 +0,0 @@
-package model.game
-
-/** Represents the list of playable games. */
-object GamesList:
-  val games: List[String] = List("Cactus")
---
+++ src/main/scala/mvc/PlayableGame.scala
@@ -0,0 +1,18 @@
+package mvc
+
+import mvc.{CactusMVC, GameMVC}
+
+/** Represents the playable of games. */
+enum PlayableGame:
+  case Cactus
+  
+  def name: String = this match
+    case Cactus => "Cactus"
+
+  /**
+   * Gets the respective game MVC.
+   *
+   * @return the game MVC.
+   */
+  def gameMVC: GameMVC = this match
+    case Cactus => CactusMVC
