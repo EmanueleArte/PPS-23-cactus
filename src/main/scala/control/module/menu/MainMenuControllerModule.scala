490208f920ac0f7d44145d775265127e50f219a2
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-22 18:58:09 +0200

    Make game start not only related to one game

--- src/main/scala/control/module/menu/MainMenuControllerModule.scala
+++ src/main/scala/control/module/menu/MainMenuControllerModule.scala
@@ -3,7 +3,7 @@ package control.module.menu
 import control.module.ControllerModule
 import model.bot.CactusBotsData.{DiscardMethods, DrawMethods, Memory}
 import model.module.menu.MainMenuModelModule
-import mvc.CactusMVC
+import mvc.{CactusMVC, PlayableGame}
 import view.module.menu.MainMenuViewModule
 
 object MainMenuControllerModule extends ControllerModule:
@@ -16,7 +16,7 @@ object MainMenuControllerModule extends ControllerModule:
      * Selects a game.
      * @param game the game selected.
      */
-    def selectGame(game: String): Unit
+    def selectGame(game: PlayableGame): Unit
 
     /**
      * Starts a game with a given number of players.
@@ -25,12 +25,12 @@ object MainMenuControllerModule extends ControllerModule:
     def startGame(nPlayers: Int): Unit
 
     /**
-     * Starts a game with the given players.
+     * Starts a game with bot players.
      * @param drawings the drawing methods of the bots.
      * @param discardings the discarding methods of the bots.
      * @param memories the memory options of the bots.
      */
-    def startGame(drawings: Seq[DrawMethods], discardings: Seq[DiscardMethods], memories: Seq[Memory]): Unit
+    def startGameWithBots(drawings: Seq[DrawMethods], discardings: Seq[DiscardMethods], memories: Seq[Memory]): Unit
 
   /** Represents the controller component for the menu. */
   trait Component:
@@ -38,15 +38,15 @@ object MainMenuControllerModule extends ControllerModule:
 
     /** Implementation of [[MainMenuController]]. */
     class MainMenuControllerImpl extends MainMenuController:
-      def selectGame(game: String): Unit = context.model.selectedGame = game
+      def selectGame(game: PlayableGame): Unit = context.model.selectedGame = game
 
       def startGame(nPlayers: Int): Unit =
-        CactusMVC.setup(nPlayers)
-        CactusMVC.run()
+        context.model.selectedGame.gameMVC.setup(nPlayers)
+        context.model.selectedGame.gameMVC.run()
 
-      def startGame(drawings: Seq[DrawMethods], discardings: Seq[DiscardMethods], memories: Seq[Memory]): Unit =
-        CactusMVC.setup(context.model.createPlayers(drawings, discardings, memories))
-        CactusMVC.run()
+      def startGameWithBots(drawings: Seq[DrawMethods], discardings: Seq[DiscardMethods], memories: Seq[Memory]): Unit =
+        context.model.selectedGame.gameMVC.setupWithBots((drawings, discardings, memories))
+        context.model.selectedGame.gameMVC.run()
 
   /** Interface of the controller module of the menu. */
   trait Interface extends Provider with Component:
