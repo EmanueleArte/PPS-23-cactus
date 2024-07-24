4afe301b0e198e88a5acd1d8a2a017f7802e06c0
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-07-24 18:17:01 +0200

    Fix CactusLogicTest after the implementation of continue

--- src/test/scala/model/logic/CactusLogicTest.scala
+++ src/test/scala/model/logic/CactusLogicTest.scala
@@ -13,7 +13,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 class CactusLogicTest extends AnyFlatSpec:
 
   val playersNumber: Int = 4
-  val deckSize: Int = 52
+  val deckSize: Int      = 52
 
   /** Custom implementation of the CactusGame to make tests with an unshuffled deck. */
   class TestCactusLogic(nPlayers: Int) extends CactusLogic(Left(nPlayers): Either[Int, BotParamsType]) with GameLogic:
@@ -22,47 +22,47 @@ class CactusLogicTest extends AnyFlatSpec:
 
   "A Cactus Logic" should "have the correct number of players after the setup" in:
     val logic = CactusLogic(playersNumber)
-    logic.players.size should be (playersNumber)
+    logic.players.size should be(playersNumber)
 
   "A player" should "draw from the deck" in:
     val logic = CactusLogic(playersNumber)
     logic.draw(true)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber + 1)
-    logic.game.deckSize should be (deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber + 1)
+    logic.game.deckSize should be(deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
 
   it should "discard a card" in:
     val logic = CactusLogic(playersNumber)
     logic.currentPhase_=(CactusTurnPhase.Discard)
     logic.discard(0)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber - 1)
-    logic.game.discardPile.size should be (1)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber - 1)
+    logic.game.discardPile.size should be(1)
 
-  it should "draw from the discard pile" in :
+  it should "draw from the discard pile" in:
     val logic = CactusLogic(playersNumber)
     logic.currentPhase_=(CactusTurnPhase.Discard)
     logic.discard(0)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber - 1)
-    logic.game.discardPile.size should be (1)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber - 1)
+    logic.game.discardPile.size should be(1)
     logic.currentPhase_=(CactusTurnPhase.Draw)
     logic.draw(false)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber)
-    logic.game.discardPile.size should be (0)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber)
+    logic.game.discardPile.size should be(0)
 
   it should "make a basic complete turn" in:
     val logic = CactusLogic(playersNumber)
     logic.draw(true)
     logic.discard(0)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber)
-    logic.game.discardPile.size should be (1)
-    logic.game.deckSize should be (deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber)
+    logic.game.discardPile.size should be(1)
+    logic.game.deckSize should be(deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
 
   it should "have a malus if he discards a card not in the classic discard phase and the discard pile is empty" in:
     val logic = CactusLogic(playersNumber)
     logic.currentPhase_=(CactusTurnPhase.DiscardEquals)
     logic.discardWithMalus(0)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber + 1)
-    logic.game.discardPile.size should be (0)
-    logic.game.deckSize should be (deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber + 1)
+    logic.game.discardPile.size should be(0)
+    logic.game.deckSize should be(deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
 
   it should "have a malus if he discards an incorrect card not in the classic discard phase" in:
     val logic = TestCactusLogic(playersNumber)
@@ -70,9 +70,9 @@ class CactusLogicTest extends AnyFlatSpec:
     logic.discard(1)
     logic.nextPlayer
     logic.discardWithMalus(1)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber + 1)
-    logic.game.discardPile.size should be (1)
-    logic.game.deckSize should be (deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber + 1)
+    logic.game.discardPile.size should be(1)
+    logic.game.deckSize should be(deckSize - playersNumber * logic.game.initialPlayerCardsNumber - 1)
 
   it should "discard a card if it matches the criteria of the non-classic discard" in:
     val logic = TestCactusLogic(playersNumber)
@@ -81,26 +81,29 @@ class CactusLogicTest extends AnyFlatSpec:
     println(logic.game.discardPile)
     for _ <- 1 to 3 do logic.nextPlayer
     logic.discardWithMalus(2)
-    logic.currentPlayer.cards.size should be (logic.game.initialPlayerCardsNumber - 1)
-    logic.game.discardPile.size should be (2)
-    logic.game.deckSize should be (deckSize - playersNumber * logic.game.initialPlayerCardsNumber)
+    logic.currentPlayer.cards.size should be(logic.game.initialPlayerCardsNumber - 1)
+    logic.game.discardPile.size should be(2)
+    logic.game.deckSize should be(deckSize - playersNumber * logic.game.initialPlayerCardsNumber)
 
   "Players" should "make a complete match using basic moves" in:
     val logic = CactusLogic(playersNumber)
     while !logic.isGameOver do
       logic.draw(true)
       logic.discard(0)
+      logic.continue()
       logic.callCactus()
       logic.nextPlayer
       logic.currentPhase_=(CactusTurnPhase.Draw)
-    logic.game.deckSize should be (deckSize - playersNumber * logic.game.initialPlayerCardsNumber - playersNumber)
-    logic.game.discardPile.size should be (playersNumber)
+    logic.game.deckSize should be(deckSize - playersNumber * logic.game.initialPlayerCardsNumber - playersNumber)
+    logic.game.discardPile.size should be(playersNumber)
 
   "At the end of a Cactus match" should "be possible to calculate the scores" in:
     val logic = CactusLogic(playersNumber)
     while !logic.isGameOver do
       logic.draw(true)
       logic.discard(0)
+      logic.continue()
       logic.callCactus()
       logic.nextPlayer
+      logic.currentPhase_=(CactusTurnPhase.Draw)
     for (_, score) <- toMap(logic.calculateScore) do score should be > 0
