16b2801df764a0be97816cf9d356464e9ccb721b
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-08-05 18:25:32 +0200

    Create tutorial pane

---
+++ src/main/scala/view/module/tutorial/TutorialPane.scala
@@ -0,0 +1,68 @@
+package view.module.tutorial
+
+import control.module.tutorial.TutorialControllerModule.TutorialController
+import mvc.PlayableGame
+import scalafx.beans.property.ReadOnlyDoubleProperty
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.layout.{Pane, VBox}
+import view.TutorialDescriptions.GameTutorialValue
+import view.Utils.CustomStackPane
+import view.module.cactus.{AppPane, ScalaFXPane}
+import view.ViewDSL.{
+  aligned,
+  bold,
+  colored,
+  containing,
+  spaced,
+  telling,
+  veryBig,
+  withMargin,
+  textFlow,
+  dynamicLong,
+  Label as LabelElement
+}
+import view.{TutorialDescriptions, ViewPosition}
+
+import scala.language.postfixOps
+
+/**
+ * ScalaFX tutorial pane for the cactus game.
+ *
+ * @param controller controller of the tutorial.
+ * @param sceneWidth width of the scene.
+ * @param sceneHeight height of the scene
+ */
+class TutorialPane(
+    game: PlayableGame,
+    controller: TutorialController,
+    sceneWidth: ReadOnlyDoubleProperty,
+    sceneHeight: ReadOnlyDoubleProperty
+) extends ScalaFXPane:
+  override def paneWidth: Int         = AppPane.mainPaneWidth
+  override def paneHeight: Int        = AppPane.mainPaneHeight
+  override def position: ViewPosition = ViewPosition(paneWidth / 2, 0)
+
+  override def pane: Pane = new CustomStackPane(sceneWidth, sceneHeight)
+    .colored(AppPane.mainPaneColor)
+    .containing(
+      new VBox()
+        .aligned(Pos.TopCenter)
+        .spaced(20)
+        .containing(
+          (LabelElement telling TutorialDescriptions.gamesDescriptions(game)(GameTutorialValue.Name) bold).veryBig
+          .aligned(Pos.TopCenter)
+          .withMargin(new scalafx.geometry.Insets(Insets(50, 0, 50, 0)))
+        )
+        .containing(
+          (LabelElement telling TutorialDescriptions
+            .gamesDescriptions(game)(GameTutorialValue.Description)).dynamicLong(sceneWidth).aligned(Pos.TopLeft).textFlow
+        )
+        .containing(
+          (LabelElement telling TutorialDescriptions
+            .gamesDescriptions(game)(GameTutorialValue.Rules)).dynamicLong(sceneWidth).aligned(Pos.TopLeft).textFlow
+        )
+        .containing(
+          (LabelElement telling TutorialDescriptions
+            .gamesDescriptions(game)(GameTutorialValue.Points)).dynamicLong(sceneWidth).aligned(Pos.TopLeft).textFlow
+        )
+    )
