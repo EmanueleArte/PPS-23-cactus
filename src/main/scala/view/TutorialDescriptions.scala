6b7a9ba795583d9abc5eaad3e3597761ba584093
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-08-05 18:12:37 +0200

    Add tutorials data

---
+++ src/main/scala/view/TutorialDescriptions.scala
@@ -0,0 +1,35 @@
+package view
+
+import mvc.PlayableGame
+
+/** Contains the tutorials of the playable games. */
+object TutorialDescriptions:
+  /** Enum with the game rules. */
+  enum GameTutorialValue:
+    case Name, Description, Rules, Points
+
+  /** Map with the tutorial for each game. */
+  val gamesDescriptions: Map[PlayableGame, Map[GameTutorialValue, String]] = Map[PlayableGame, Map[GameTutorialValue, String]](
+    PlayableGame.Cactus -> Map[GameTutorialValue, String](
+      GameTutorialValue.Name -> (PlayableGame.Cactus.name + " Tutorial"),
+      GameTutorialValue.Description -> ("Cactus is a card game based on memory. The goal is to have less points in your hand than the other players.\n" +
+        "The game is played with a Poker deck. The game ends when anyone has done another turn after the call of \"Cactus\" made by a player."),
+      GameTutorialValue.Rules -> ("The game is played in turns, where each player has to draw a card from the deck or the discard pile, watch it and then discard one from his hand. " +
+        "Some cards when discarded in the latter phase activate special effects. \nAfter the previous discarding and the, eventual, effect resolution there is a phase where all the players" +
+        "can discard a card with the same value of the one on top of the discard pile. \nIf they don't want to, they can proceed with the game." +
+        "Before the end of his turn the player can call \"Cactus\".\n" +
+        "At the start of the first turn each player has to watch two cards in his hand and remember them.\n" +
+        "Every card added to a player's hand is placed as last item of the hand.\n" +
+        "Some cards have special effects: " +
+        "\n- Aces, when discarded, allows the player to choose another player that has to draw a card from the deck without seeing it." +
+        "\n- Jacks, when discarded, allows the player to see a card in his hand."
+        ),
+      GameTutorialValue.Points -> ("The points are calculated as follows: " +
+        "\n- Number cards have their value as points (e.g. Ace -> 1 point, Two of Spades -> 2 points, ...)." +
+        "\n- Jacks are 11 points." +
+        "\n- Queens are 12 points." +
+        "\n- Kings are 13 points." +
+        "\n- Red Kings have 0 points."
+        )
+    )
+  )
--- src/main/scala/view/Utils.scala
+++ src/main/scala/view/Utils.scala
@@ -9,8 +9,8 @@ import scalafx.scene.paint.Color
 
 /** Contains utility methods for the view. */
 object Utils:
-  val topPosition: Int = 0
-  val leftPosition: Int = 0
+  val topPosition: Int            = 0
+  val leftPosition: Int           = 0
   val topLeftCorner: ViewPosition = ViewPosition(topPosition, leftPosition)
 
   /** Class for a turn phase, with a name and a description. */
@@ -18,15 +18,26 @@ object Utils:
 
   /** Map with the names and descriptions to show in the GUI, for each turn phase. */
   val turnPhaseDescription: Map[TurnPhase, Phase] = Map[TurnPhase, Phase](
-    BaseTurnPhase.Start -> Phase("Watch cards", "Click 2 cards to see them."),
+    BaseTurnPhase.Start  -> Phase("Watch cards", "Click 2 cards to see them."),
     CactusTurnPhase.Draw -> Phase("Draw", "Draw one card from the deck or from the discard pile, by clicking on it."),
     CactusTurnPhase.Discard -> Phase("Discard", "Choose a card from your hand to discard."),
-    CactusTurnPhase.DiscardEquals -> Phase("Discard equals cards", "You can discard a card with the same value of the one on top of the discard pile. If you don't want, you can click on continue.\n\nRemember: discarding a card with a different value, will make you draw a card from the deck."),
-    CactusTurnPhase.DiscardEquals -> Phase("Discard equals cards", "You can discard a card equals to the one on top of the discard pile."),
-    CactusTurnPhase.EffectActivation -> Phase("Effect activation", "Activate the effect of the card you just discarded."),
-    CactusTurnPhase.AceEffect -> Phase("Ace discarded", "Choose a player and have them draw a card from the deck."),
+    CactusTurnPhase.DiscardEquals -> Phase(
+      "Discard equals cards",
+      "You can discard a card with the same value of the one on top of the discard pile." +
+        "If you don't want, you can click on continue.\n\n" +
+        "Remember: discarding a card with a different value, will make you draw a card from the deck."
+    ),
+    CactusTurnPhase.DiscardEquals -> Phase(
+      "Discard equals cards",
+      "You can discard a card equals to the one on top of the discard pile."
+    ),
+    CactusTurnPhase.EffectActivation -> Phase(
+      "Effect activation",
+      "Activate the effect of the card you just discarded."
+    ),
+    CactusTurnPhase.AceEffect  -> Phase("Ace discarded", "Choose a player and have them draw a card from the deck."),
     CactusTurnPhase.CallCactus -> Phase("Cactus", "Call \"Cactus\" and end the game."),
-    BaseTurnPhase.End -> Phase("End", "End of the turn. Click \"Continue\" and proceed with the game.")
+    BaseTurnPhase.End          -> Phase("End", "End of the turn. Click \"Continue\" and proceed with the game.")
   )
 
   extension (color: Color)
@@ -35,16 +46,15 @@ object Utils:
      * @return string representation of the color.
      */
     def toRgbString: String =
-      val red = (color.red * 255).toInt
+      val red   = (color.red * 255).toInt
       val green = (color.green * 255).toInt
-      val blue = (color.blue * 255).toInt
+      val blue  = (color.blue * 255).toInt
       val alpha = color.opacity
 
       if (alpha == 1.0)
         s"rgb($red, $green, $blue)"
       else
         s"rgba($red, $green, $blue, $alpha)"
-
 
   /**
    * Gets the value of the combo box.
